f4e2fe8d9e36e6061e0ed8592ba872d943b9a059
nguyenloan <20020114@vnu.edu.vn>
2022-12-01 16:22:20 +0700

    complete create hotel

--- frontend/src/apis/hotelApi.js
+++ frontend/src/apis/hotelApi.js
@@ -1,4 +1,5 @@
-import { GET_PROVINCES_URL } from "../configs/api";
+import axios from "axios";
+import { GET_PROVINCES_URL, CREATE_HOTEL_URL } from "../configs/api";
 
 export const getAllProvinces = (setAll) => {
   fetch(GET_PROVINCES_URL)
@@ -10,4 +11,22 @@ export const getAllProvinces = (setAll) => {
     });
 };
 
-export const createHotelApi = () => {};
+export const createHotelApi = (data, tokenStr) => {
+  const options = {
+    headers: {
+      Authorization: `Bearer ${tokenStr}`,
+    },
+  };
+  const response = axios
+    .post(CREATE_HOTEL_URL, data, options)
+    .then((res) => {
+      console.log("RESPONSE ==== : ", res);
+      return res.data;
+    })
+    .catch((err) => {
+      console.log("ERROR: ====", err);
+      return err.response.data.Message;
+    });
+
+  return response;
+};
