74680165d5fb1866fe5a49379bea1fab9c7fa6a4
nguyenloan <20020114@vnu.edu.vn>
2022-12-04 00:20:20 +0700

    add infor room modal

--- frontend/src/configs/api.js
+++ frontend/src/configs/api.js
@@ -43,3 +43,5 @@ export const UPDATE_HOTEL_URL = `${DOMAIN_URL}/hotel/update`;
 // API for room------------------------------------------------
 
 export const CREATE_ROOM_URL = `${DOMAIN_URL}/room/create`;
+
+export const GET_ALL_ROOMS_URL = `${DOMAIN_URL}/room/list`;
