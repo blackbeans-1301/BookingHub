541971d78a0b2c08fde4bb0130ccbfc9ab6bb81a
blackbeans-1301 <blackbeans.1301@gmail.com>
2022-12-16 20:09:44 +0700

    sua server de lay duoc data

--- backend/sever.js
+++ backend/sever.js
@@ -12,7 +12,7 @@ var corsOptions = {
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/*', function (req, res) {
+app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'public/index.html'))
 })
 
