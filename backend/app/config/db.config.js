8fbaa0ba44be75f382f0e7bb3fea3a36fefd470c
Vũ Thanh Lâm <79557871+FakeMG@users.noreply.github.com>
2022-12-21 03:32:56 +0700

    refractor: move db info, baseURL to env

--- backend/app/config/db.config.js
+++ backend/app/config/db.config.js
@@ -1,16 +1,4 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "",
-    DB: "booking",
-    // PASSWORD: "password",
-    // DB: "db1",
     dialect: "mysql",
-    timezone: "Asia/Ho_Chi_Minh",
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
+    timezone: "Asia/Ho_Chi_Minh"
 }
