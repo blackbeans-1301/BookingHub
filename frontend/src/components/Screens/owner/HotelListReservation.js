d0518ff918e9aafd1179e6979a6c702ae092b79e
blackbeans-1301 <blackbeans.1301@gmail.com>
2022-12-21 09:47:31 +0700

    chinh view

--- frontend/src/components/Screens/owner/HotelListReservation.js
+++ frontend/src/components/Screens/owner/HotelListReservation.js
@@ -100,7 +100,7 @@ const HotelListReservation = () => {
                   <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
                     Status
                   </th>
-                  <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                  <th className="px-3 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
                     Actions
                   </th>
                   <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
